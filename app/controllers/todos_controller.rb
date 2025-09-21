250b60e26ab5f2c1b7bc5e32badf07db919d06b3
Sagar <nimokam7@gmail.com>
2025-09-21 16:57:13 +0545

    feat: add controller actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,52 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
   def index
+    @todos = Todo.all.order(created_at: :desc)
   end
 
   def show
+    # @todo is set by before_action
   end
 
   def new
+    @todo = Todo.new
   end
 
   def create
+    @todo = Todo.new(todo_params)
+    
+    if @todo.save
+      redirect_to @todo, notice: 'Todo was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    # @todo is set by before_action
   end
 
   def update
+    if @todo.update(todo_params)
+      redirect_to @todo, notice: 'Todo was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @todo.destroy
+    redirect_to todos_url, notice: 'Todo was successfully deleted.'
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :completed)
   end
 end
